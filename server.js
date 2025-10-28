6a9ec92a0aa75286a4c208aa524895ef7fd68b00
vasplustechnologies <vasplustechnologies@gmail.com>
2025-10-28 14:16:00 +0300

    port change from 3000 to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet());
